b37f13816151a7a338b44fc1a91203ee7b7ffd4c
fernanda.dssan <fernanda.dssan@gmail.com>
2017-11-18 14:00:29 -0500

    meu primeiro commit.

--- index.php
+++ index.php
@@ -8,9 +8,151 @@
     <body>
                         
         <?php
+      
+      
+      
+      /*$a = 1;
+      $b = 3;
+      
+      if ($a < $b)  {
+        echo "a é menor que b";
+      } else if ($a == $b) {
+      }  else {
+        echo "a é maior que b";
+        }*/
+      
+      /*$i = 1;
+      
+      while($i < 10){
+        $i = $i + 1;
+        echo $i . "<br>";
+        
+      }*/
+      
+      /*$i = 1;
+      
+     while ($i < 10) {
+       $i = $i + 1;
+ }       if ($i%2 == 0) {
+         echo "o valor de $i é par <br>";
+       }else{ 
+         echo "o valor $i é ímpar <br>";
+     } 
+     }*/
+      
+      
+      /*$i = 1;
+      
+      for ($i = 1; <= 10; $i++) {
+        echo " o valor de i é igual : $i <br>";
+      }*/
+      
+      /*$nome = "Júlio";
+      switch ($nome) {
+        case "Ana";
+          echo "Olá Ana";
+          break; //comentário não vai testar os demais cases; vai parar por aí; senão vai ficar testando até dar defaul
+          
+          case "Pedro";
+            echo "Olá Pedro";
+          break;
+          
+        default:
+          echo "Não sei quem você é";
+      }*/
+      
+      /*function multiplicacao($a, $b) {
+        $resultado = $a * $b;
+        return $resultado;
+      }
+      
+      $numero1 = 23;
+      $numero2 = 20;
+      
+      $resultadoFuncao = multiplicacao($numero1, $numero2);
+      
+      echo $resultadoFuncao;*/
+      
+      /*$cores = ["vermelho", "azul"];
+      echo $cores[4];*/
+      
+      /*$pessoa = ["nome" => Ana, "idade" => 23];
+      echo $pessoa ["idade"];*/
+      
+      /*$numeros = [1, 2, 3];
+      array_push($numeros, 4);
+      echo $numeros [3];*/
+      
+      /*$frutas = array("maça", "laranja", "morango");
+      foreach($frutas as $fruta) {
+        echo "A fruta é $fruta <br>";
+      }*/
+      
+      $comidas = array ("salsicha", "batata", "ovo");
+      foreach ($comidas as $comida) {
+        echo "Minha comida favorita é $comida <br>";
+      }
+      
+     array_pop($comidas); 
+      foreach ($comidas as $comida) {
+       echo "Minha comida favorita é $comida <br>";
+      }
+      
+     /* if ($a > Sb) {
+        echo "a é maior que b";
+      } else if ($a == $b) {
+        echo "a é igual a b";
+      } else {
+        echo "a é menor que b";
+      }*/
+      
+      /*$nome = "Ana";
+      echo "Olá " . $nome; //comentário a concatenação soma as variáveis no site = OLÁ + ANA*/
+      
+     /*$nome = "Ana";
+      echo "Olá" . $nome . "!";*/
+      
+      /*$a = 3;
+      $b = 5;
+      $c = 2;*/
+      
+     
+      
+      /*$resultado = $c + $b;*/
+      
+      /*$resultado = $a ** $b;*/
+      
+      /*$resultado = $a * $b;*/
+      
+      /*$resultado = $a / $b;*/
+      
+     /* $resultado = $a + $b;*/
+      
+      /*$resultado = $a + $b;*/
+      
+      echo $resultado;
+      
+      
+      
+      /*$cor = "amarelo";
+        var_dump($cor);*/
+      
+      /*var_dump (12);*/
+      
+      /*var_dump (array("maça", "maracujá"))*/
+      
+            /*$cor = "amarelo"; //comentario  sempre encerrar uma instrução com ponto e vírgula
+            $cor = "vermelho"; //comentario isso anula o amarelo porque o amarelo não foi definido como uma variável constante
+            echo $cor; //essa instrução vai habilitar o comando com o nome da cor 
+      
+      //usar control S para salvar*/
+      
 
-            // Coloque seu código aqui!
+        ?>
+        <?php
 
+      
+           
         ?>
 
     </body>
